2692b18a695446aa5ab067809adf43665aff7bd8
MHLoyche <MHLoyche@outlook.com>
2026-04-21 11:01:31 +0200

    fixxed routing, sidebar and robots.txt

--- helpflow-frontend/src/app/app.routes.ts
+++ helpflow-frontend/src/app/app.routes.ts
@@ -6,14 +6,36 @@ import { Articles } from './pages/articles/articles';
 import { SlaManagement } from './pages/sla-management/sla-management';
 
 export const routes: Routes = [
-    {
-    path: '',
+  {
+    path: "",
     component: MainLayout,
     children: [
-        { path: 'dashboard', component: Dashboard },
-        { path: 'tickets', component: Tickets },
-        { path: 'articles', component: Articles },
-        { path: 'sla-management', component: SlaManagement }
-    ]
-    }
+        {
+            path: "",
+            pathMatch: "full",
+            redirectTo: "dashboard",
+        },
+        {
+            path: "dashboard",
+            loadComponent: () =>
+            import("./pages/dashboard/dashboard").then((m) => m.Dashboard),
+        },
+        {
+            path: "tickets",
+            loadComponent: () =>
+            import("./pages/tickets/tickets").then((m) => m.Tickets),
+        },
+        {
+            path: "articles",
+            loadComponent: () =>
+            import("./pages/articles/articles").then((m) => m.Articles),
+        },
+        {
+            path: "sla-management",
+            loadComponent: () =>
+            import("./pages/sla-management/sla-management").then((m) => m.SlaManagement),
+        },
+        ],
+    },
+    {path: "**", redirectTo: ""},
 ];
