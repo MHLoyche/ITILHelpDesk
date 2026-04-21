0a96610ed39031d9acba962cbd1713b157e88097
MHLoyche <MHLoyche@outlook.com>
2026-04-21 14:04:06 +0200

    Added article API

--- helpflow-backend/routes/articles.js
+++ helpflow-backend/routes/articles.js
@@ -2,20 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/connection');
 
-// Get all tickets with status name
+// Get all articles
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT 
-        t.ticket_id,
-        t.title,
-        t.requesterName,
-        t.createdAt,
-        t.status_id,
-        s.name AS statusName
-      FROM tickets t
-      LEFT JOIN status s ON t.status_id = s.status_id
-      ORDER BY createdAt DESC
+      SELECT * FROM articles
     `);
 
     res.json(rows);
--- helpflow-backend/server.js
+++ helpflow-backend/server.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 
+// Imports
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Route imports
 const ticketRoutes = require('./routes/tickets');
+const articlesRoutes = require('./routes/articles');
 
+// Using routes
+app.use('/api/articles', articlesRoutes);
 app.use('/api/tickets', ticketRoutes);
 
+// Start server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
