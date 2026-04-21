aaa739201e7b5d814558b3f0872327b6231c79f8
MHLoyche <MHLoyche@outlook.com>
2026-04-21 14:27:20 +0200

    Get article by ID - some article HTML - Unfinished

--- helpflow-backend/routes/articles.js
+++ helpflow-backend/routes/articles.js
@@ -6,7 +6,18 @@ const db = require('../db/connection');
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT * FROM articles
+        SELECT 
+            a.article_id,
+            a.title,
+            a.content,
+            a.createdAt,
+            a.updatedAt,
+            a.authorName,
+            a.category_id,
+            c.name AS categoryName
+        FROM articles a
+        LEFT JOIN categories c ON a.category_id = c.category_id
+        ORDER BY createdAt DESC
     `);
 
     res.json(rows);
@@ -14,5 +25,35 @@ router.get('/', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// Get article by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT 
+          a.article_id,
+          a.title,
+          a.content,
+          a.createdAt,
+          a.updatedAt,
+          a.authorName,
+          a.category_id,
+          c.name AS categoryName
+      FROM articles a
+      LEFT JOIN categories c ON a.category_id = c.category_id
+      WHERE a.article_id = ?
+      LIMIT 1
+    `, [req.params.id]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Article not found" });
+    }
+
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 
 module.exports = router;
--- helpflow-frontend/src/app/pages/articles/articles.ts
+++ helpflow-frontend/src/app/pages/articles/articles.ts
@@ -1,9 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, signal } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { ArticleService } from "../../services/articles";
 
 @Component({
   selector: "app-articles",
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: "./articles.html",
   styleUrl: "./articles.css",
 })
-export class Articles {}
+export class Articles implements OnInit{
+  articles = signal<any[]>([]);
+  loading = signal(true);
+  error = signal<string | null>(null);
+  
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.articleService.getArticles().subscribe({
+      next: (data) => {
+        this.articles.set(data ?? []);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set("Failed to load articles");
+        this.loading.set(false);
+      },
+    });
+  }
+}
